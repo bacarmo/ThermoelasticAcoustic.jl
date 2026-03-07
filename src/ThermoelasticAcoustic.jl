5ea4c230194e0485cc341cc05b300fd9a839897b
Bruno Alves do Carmo <bruno.carmo@ppgi.ufrj.br>
2026-03-06 22:10:08 -0300

    feat: update imports, exports, and includes in main module

--- src/ThermoelasticAcoustic.jl
+++ src/ThermoelasticAcoustic.jl
@@ -1,8 +1,29 @@
 module ThermoelasticAcoustic
 
+using StaticArrays: SVector, SMatrix
+using GaussQuadrature: legendre
+using Printf: @printf
+using SparseArrays: sparse, spzeros, SparseMatrixCSC
+using LinearAlgebra: Symmetric, lmul!, cholesky, ldiv!, mul!
+
 # Exports
-export PDEInputData, example1_manufactured
+export PDEInputData, example1_manufactured, example1_zero_source, example2_manufactured,
+       example2_zero_source
+export FEMState, L2ErrorCallback
+export Lagrange, Hermite
+export CrankNicolson, ModifiedCN
+export pde_solve
+export convergence_study_coupled, convergence_study_spatial, convergence_study_temporal
+export print_convergence_table
 
 # Includes
 include("pde_inputdata.jl")
+include("mesh/mesh.jl")
+include("fem/fem.jl")
+include("callbacks/callbacks.jl")
+include("assembly/assembly.jl")
+include("initial_solution.jl")
+include("ode_solvers/ode_solvers.jl")
+include("pde_solve.jl")
+include("postprocessing/postprocessing.jl")
 end
