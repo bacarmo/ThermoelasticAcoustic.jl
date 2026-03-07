b6ff1b83065062c4e7122e5296880dc59b44d4ca
Bruno Alves do Carmo <bruno.carmo@ppgi.ufrj.br>
2026-03-06 21:50:00 -0300

    feat: define initial signatures for ModifiedCN and CrankNicolson ODE solvers

---
+++ src/ode_solvers/modified_crank_nicolson.jl
@@ -0,0 +1,16 @@
+function ode_solve(
+        ::ModifiedCN,
+        state::FEMState{T},
+        matrices::SystemMatrices,
+        dof_map_m₁::DOFMap,
+        dof_map_m₂::DOFMap,
+        dof_map_m₃::DOFMap,
+        mesh1D::CartesianMesh{1, I},
+        mesh2D::CartesianMesh{2, I},
+        quad::QuadratureSetup,
+        tspan::StepRangeLen{T},
+        input_data::PDEInputData,
+        callback::AbstractCallback
+) where {T <: AbstractFloat, I <: Integer}
+    return callback
+end
