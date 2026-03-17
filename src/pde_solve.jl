681d2d9e9ca3167d0690b610489e67ee7926ceec
Bruno Alves do Carmo <bruno.carmo@ppgi.ufrj.br>
2026-03-16 21:02:42 -0300

    fix: use tau in build_cache

--- src/pde_solve.jl
+++ src/pde_solve.jl
@@ -76,7 +76,8 @@ function pde_solve(
     # ========================================
     # Compute vⁿ, dⁿ, rⁿ, and zⁿ for n ≥ 1
     # ========================================
-    cache = build_cache(solver, matrices, dof_map_m₁, dof_map_m₂, dof_map_m₃)
+    τ = T(step(tspan))
+    cache = build_cache(solver, matrices, dof_map_m₁, dof_map_m₂, dof_map_m₃, τ)
     ode_solve(
         solver,
         cache,
